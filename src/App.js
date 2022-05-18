fc6cabf6748d4c274f5d0cef8dcb3296534564f2
gyungsubLee <dltjq2323@gamil.com>
2022-05-18 13:02:13 +0900

    code/chapter6.2/useEffect_mount #5

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React, { useState, useEffect } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter(pre => pre + 1);
-  console.log("call an api");
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <React.Fragment>
       <h1>{counter}</h1>
