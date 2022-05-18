0f1260d868c0a484b0ac34c19a4ea40f255ea3bc
gyungsubLee <dltjq2323@gamil.com>
2022-05-18 18:45:53 +0900

    code/chapter6.4/useEffect(upmount) #5

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import React, { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(()=>{
+    console.log("Im here!");
+    return () => {
+      console.log("bye");
+    }
+  }, [])
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [keyword, setKeyword] = useState("");
-  const [counter, setCounter] = useState(0);
-
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setCounter(pre => pre + 1);
-
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  
-  useEffect(() => {
-    console.log("CALL THE API...");
-  }, []);
-
-  useEffect(()=>{
-    if (keyword !== "" && keyword.length > 5){
-      console.log("SEARCH FOR", keyword);
-    } 
-  }, [keyword]);
-
-  useEffect(()=>{
-    console.log("I run when 'counter' change.");
-  }, [counter]);
-
-  useEffect(() => {
-    console.log("I run when 'counter' && 'counter' change.");
-  }, [counter, keyword])
-  
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(pre => !pre);
   return (
     <React.Fragment>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </React.Fragment>
   );
 }
