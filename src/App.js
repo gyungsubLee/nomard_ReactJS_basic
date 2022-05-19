82e3b9aea72e0868711a64d989f1b7eff4ee09de
gyungsubLee <dltjq2323@gamil.com>
2022-05-20 01:45:34 +0900

    README 수정

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   const [movies, setMovies] = useState([]);
 
   const getMovies = async() => {
-    const response = await ( await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    )).json()
-    setMovies(response.data.movies);
+    const json = await ( 
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
     setLoading(false);
   };
 
