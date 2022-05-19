9071a77d5ae76c2319c230fc1b0c0029da58831b
gyungsubLee <dltjq2323@gamil.com>
2022-05-19 22:10:32 +0900

    code/chapter7.2/challenge #6

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [amount, setAmount] = useState("");
+  const [inverted, setInverted] = useState(false);
 
-  const onChange = event => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === ""){
-      return;
-    }
-    setToDos(currentArray => [...currentArray, todo]);
-    setTodo("");
+  const onChange = (event) =>{
+    setIndex(event.target.value);
   }
-  const onClick = () => {
-    setToDos([]);
+  const _onChange = (event) => {
+    setAmount(event.target.value);
   }
+  const reset = () => setAmount("");
+  const onInverted = () => {
+    setInverted(current => !current);
+    reset();
+  }
+  const coin = coins[index]
+  const coin_price = coin?.quotes.USD.price
 
-  const toDos_local = JSON.parse(localStorage.getItem("todos"));
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify([...toDos]))
-  },  [toDos]);
-
-  useEffect(() => {
-    setToDos(toDos_local);
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json)
+        setLoading(false);
+      });
   }, []);
 
   return (
     <React.Fragment>
-      <form onSubmit={onSubmit}>
-        <input 
-          placeholder="please write your todo"
-          value={todo}
-          onChange={onChange}
+      <h1>The Coins!{loading? "" : `(${coins.length})` }</h1>
+      {loading? (
+        <strong>loading...</strong>
+      ) : (
+        <select value={index} onChange={onChange}>
+          {coins.map((coin, idx) => (
+            <option value={idx} key={idx}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <hr />
+      <div>
+        <input
+          type="number"
+          placeholder="price"
+          value={inverted? amount * coin_price : amount}
+          onChange={_onChange}
+          disabled={inverted}
         />
-        <input type="submit" value="submit" />    
-        <button onClick={onClick}>reset</button>
-      </form>
-      {toDos.map((item, idx)=>(
-            <li key={idx}>{item}</li>
-        ))}
+      </div>
+      <div>
+        <input
+          type="number"
+          placeholder={coin?.name}
+          value={inverted? amount : amount/coin_price}
+          onChange={_onChange}
+          disabled={!inverted}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onInverted}>{inverted? "Turn back" : "invert"}</button>
     </React.Fragment>
   );
 }
