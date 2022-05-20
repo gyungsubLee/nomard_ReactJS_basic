aa9d3c71709c747cd7cc5ec603afac8b8d059a68
gyungsubLee <dltjq2323@gamil.com>
2022-05-20 13:05:02 +0900

    code/chapter7.4/component_Movie_props-types #6

--- src/Movie.js
+++ src/Movie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types"
 
 
 function Movie ({ coverImg, title, summary, genres }) {
@@ -16,6 +17,14 @@ function Movie ({ coverImg, title, summary, genres }) {
             </ul>
         </React.Fragment>
     )
+}
+
+Movie.propTypes = {
+    coverImg: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+
 }
 
 export default Movie;
